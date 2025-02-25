56902a48f6b2c112cfac26d4bcb0ec2278505413
Nkln <nkln@yahoo.com>
2025-02-25 08:13:43 +0000

    Assistant checkpoint: Hide counter when value is 0
    Assistant generated file changes:
    - client/src/components/client-dashboard/ReceivedOffersTab.tsx: Hide counter when 0
    
    ---
    
    User prompt:
    
    in client dashboard, oferte primite tab, In oferte primite navifation bar it shows 0 the counter if no new offer. should be hide if 0

---
+++ client/src/components/client-dashboard/ReceivedOffersTab.tsx
@@ -0,0 +1,3 @@
+<TabsTrigger value="new" className="data-[state=active]:bg-[#00aff5] data-[state=active]:text-white">
+            Oferte Noi {newOffersCount > 0 && `(${newOffersCount})`}
+          </TabsTrigger>
